d9a8beab713f9fffaba2de3cfac594bd58a91d3d
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-06-12 11:47:15 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,19 +3,19 @@
  * _puts_recursion prints a string
  * @s: input of the string
  *
- *Return:0 if succeful
+ *return void
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-		if (*s)
+	_putchar(*s);
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(10);
+		return;
 	}
-		else
-		_putchar('/n')
+	s++;
+	_puts_recursion(s);
 }
-
 
 	
 
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,27 @@
 #include"main.h"
 /**
- * _print_rev_recursion reverse of a string
- * @s:output of the sring
+ * _print_rev_recursion of a string
+ * @s:input of the string
+ *return :Alway 0 (success)
  */
-void _print_rev_recursion(char *s);
-{
-	if (*s)
-	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
-	}
-}
 
+void _print_rev_recursion(char *s);
+
+{
+	int = i, len
+		len = strl(str *s)
+		if (i < len / 2)
+		{
+			str *s[i] = str *s[len - i - 1]
+			revstr(str *s)
+		}
+	int main(void)
+	{
+		char *s
+			printf("alx school")
+			gets(str *s)
+			printf("before the reverse:%s\n", str *s)
+			printf("after reversing the string:%s", str *s)
+	}
+
+}
