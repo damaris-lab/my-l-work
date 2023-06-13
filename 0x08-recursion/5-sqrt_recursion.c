a59ad4892856eca45e66393fc3b83d4621323087
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-06-12 22:14:01 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to calculate the square root of
+ * @n: number to calculate the square root from
  *
- * Return: the resulting square root
+ *return square root or -1
  */
 int _sqrt_recursion(int n)
 {
