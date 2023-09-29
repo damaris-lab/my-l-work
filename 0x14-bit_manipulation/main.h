ced109e18ef0b002f8387ecabe561be57e5060af
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-09-29 13:42:01 -0700

    print

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+int _putchar(char c);
+unsigned int binary_to_unit(const char *b);
+void print_binary(unsigned long int n);
+#endif
