c0a093da2b2581820e3818e155cfced2410c62d6
damaris-lab <damarisnyawiragacheru@gmail.com>
2023-10-01 15:48:18 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *get_bit - returns a bit at a given value
+*@n:binary no
+*@index: index of the bit
+*Return:the value at index,on error -1
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int k;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	k = 1UL << index;
+
+	if ((n & k) != 0)
+		return (1);
+	else
+		return (0);
+}
